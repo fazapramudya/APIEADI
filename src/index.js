f45f7c9b5d9e4c8c6bf61d8fc9165f5f4fc35c60
fazapramudya <95120496+fazapramudya@users.noreply.github.com>
2023-06-15 08:49:07 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,15 +21,15 @@ app.use(express.json());
 // app.use(middlewareLogs.middle2);
 
 // app.use("/users", routeUser);
-const posts =[
-    {
-        username : 'Kyle',
-        title : 'post 1'
-    }
-]
-app.get('/posts', authenticateToken, (req, res)=>{
-    res.json(posts.filter(post => post.username === req.user.name));
-})
+// const posts =[
+//     {
+//         username : 'Kyle',
+//         title : 'post 1'
+//     }
+// ]
+// app.get('/posts', authenticateToken, (req, res)=>{
+//     res.json(posts.filter(post => post.username === req.user.name));
+// })
 
 app.use("/signup", routeUser);
 
@@ -39,17 +39,20 @@ app.use("/anak", child);
 
 app.use("/login", authentication);
 
-function authenticateToken(req, res, next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null) return res.sendStatus(401);
+// function authenticateToken(req, res, next){
+//     const authHeader = req.headers['authorization'];
+//     const token = authHeader && authHeader.split(' ')[1];
+//     if(token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
-}
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+//         if (err) return res.sendStatus(403)
+//         req.user = user
+//         next()
+//     })
+// }
+app.get("*", (_req, res) => {
+	res.status(404).send("Not found");
+});
 
 app.listen(PORT, () => {
   console.log(`Server berhasil di running di port ${PORT}`);
